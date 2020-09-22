1510de4ef004b14b697ad036fbddfdfae0c0ceba
Osmany Corteguera <osmanycorteguera@gmail.com>
2020-09-22 01:33:54 -0400

    Complete pset #12.

--- psets/12-monads/Risk.hs
+++ psets/12-monads/Risk.hs
@@ -3,6 +3,8 @@
 module Risk where
 
 import Control.Monad.Random
+import Data.List
+import Foreign.Marshal.Utils (fromBool)
 
 ------------------------------------------------------------
 -- Die values
@@ -26,3 +28,51 @@ die = getRandom
 type Army = Int
 
 data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
+
+-- Exercise 2 --
+battle :: Battlefield -> Rand StdGen Battlefield
+battle bf@(Battlefield att def) = 
+  let battleAtt = min (att - 1) 3
+      battleDef = min def 2
+  in rollN battleAtt >>= \attRolls ->
+     rollN battleDef >>= \defRolls ->
+     return $ decideBattle attRolls defRolls bf
+
+
+rollN :: Int -> Rand StdGen [DieValue]
+rollN 0 = return []
+rollN n = die         >>= \x  ->
+          rollN (n-1) >>= \xs ->
+          return (x:xs)
+
+
+decideBattle :: [DieValue] -> [DieValue] -> Battlefield -> Battlefield
+decideBattle attRolls defRolls (Battlefield att def) = 
+  let battleResults = zipWith (<=)
+                              (reverse . sort $ attRolls)
+                              (reverse . sort $ defRolls)
+      deadDef       = length $ filter id battleResults
+      deadAtt       = length battleResults - deadDef
+  in Battlefield (att - deadAtt) (def - deadDef) 
+
+-- Exercise 3 --
+invade :: Battlefield -> Rand StdGen Battlefield
+invade bf@(Battlefield att def) 
+  | att < 2 || def == 0 = return bf
+  | otherwise           = battle bf >>= invade
+
+-- Exercise 4 --
+attWinRate :: Foldable t => t Battlefield -> Double
+attWinRate bfs =
+  let wins  = foldl' (\z -> (+z) . fromBool . (== 0) . defenders) 0 bfs
+      total = length bfs
+  in fromIntegral wins / fromIntegral total
+
+successProb :: Battlefield -> Rand StdGen Double 
+successProb bf = 
+  replicateM 1000 (invade bf) >>= \bfs ->
+  return $ attWinRate bfs
+
+main :: IO ()
+main = evalRandIO (successProb (Battlefield 3 3)) >>= \w ->
+       putStrLn $ show w
